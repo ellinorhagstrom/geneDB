1c7da757677a95104329e47fdf425ddb0cec4254
ellinorhagstrom <gushagels@gmail.com>
2026-09-10 14:59:35 +0200

    Added validation, GC content and messages

--- forms/geneRoute.js
+++ forms/geneRoute.js
@@ -2,26 +2,61 @@ const express = require('express');
 const Gene = require('../models/gene');
 const router = express.Router();
 
-
-// GET route for addGene page
+// GET: Render form initially with empty messages (for now the homwscreen)
 router.get('/', (req, res) => {
-    res.render('addGene', { message: null, error: null });
+    res.render('addGene', {
+        message: null,
+        error: null,
+        gcContent: null
+    });
 });
 
 // POST route for adding a gene
-router.post('/addGene', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
+        const { geneName, species, fastaSeq } = req.body;
+
+        //Validation for geneName to not be number
+        if (!isNaN(species) && species.trim() !== "") {
+            return res.render('addGene', {
+                message: null,
+                error: 'Gene name cannot be a number.',
+                gcContent: null
+            });
+        }
+
+        //Validates that sequences is only of real nucleotides
+        const nucleotideRegex = /^[ATCGU]+$/i;
+        if (fastaSeq && !nucleotideRegex.test(fastaSeq.trim())) {
+            return res.render('addGene', {
+                message: null,
+                error: 'Invalid sequence: Must only contain A, T, C, G, or U characters.',
+                gcContent: null
+            });
+        }
+
+
         const newGene = new Gene({
-            geneName: req.body.geneName,
-            species: req.body.species || undefined,
-            fastaSeq: req.body.fastaSeq,
+            geneName: geneName,
+            species: species || undefined,
+            fastaSeq: fastaSeq
         });
-        await newGene.save();
-        res.render('addGene', { message: 'Gene successfully saved!' });
+
+        const savedGene = await newGene.save(); //calculates the pre('save') method
+
+        res.render('addGene', {
+            message: 'Gene successfully added to the database!',
+            error: null,
+            gcContent: savedGene.gc_content
+        });
     } catch (err) {
-        console.error('Error saving gene:', err);
-        res.status(500).render('addGene', { error: 'Error saving gene' });
+        res.render('addGene', {
+            message: null,
+            error: 'Failed to add gene: ' + err.message,
+            gcContent: null
+        });
     }
 });
+
 
 module.exports = router;
