e17eedb7fe7d38fc034df0e061688d58eef2955e
ellinorhagstrom <gushagels@gmail.com>
2026-09-14 09:12:38 +0200

    Added comment

--- forms/displayGenesRoutes.js
+++ forms/displayGenesRoutes.js
@@ -3,12 +3,11 @@ const Gene = require('../models/gene');
 const display_router = express.Router();
 
 
-
 display_router.get("/", async function (req, res) {
     try {
+        //Finds all genes that are in the database
         const allDetails = await Gene.find({});
 
-        // Key MUST match the variable name in EJS (genes)
         res.render("displayGenes", { genes: allDetails });
     } catch (err) {
         console.log(err);
